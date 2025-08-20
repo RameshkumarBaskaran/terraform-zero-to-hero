cd89f7d5ecd233a1e0a778f95b5a6f7d9ec84f9f
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2025-08-20 19:37:16 +0530

    Update main.tf updated subnet id

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -4,4 +4,5 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-020cba7c55df1f615"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = subnet-07e075bf8cb1ffb0a
 }
