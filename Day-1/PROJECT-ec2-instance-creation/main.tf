b5206f8189205b9b60a8c466e6f556722a5c64b8
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2025-08-20 20:21:57 +0530

    Update main.tf updating region us-east-1c

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1c"  # Set your desired AWS region
 }
 resource "aws_instance" "example" {
     ami           = "ami-020cba7c55df1f615"  # Specify an appropriate AMI ID
